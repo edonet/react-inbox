d37ce9428b82d1d767fcc873106933bb2233ffa6
edonet <edonet@163.com>
2020-02-16 10:30:57 +0800

    feat: update observer

--- lib/observer.ts
+++ lib/observer.ts
@@ -46,52 +46,6 @@ const stack = new Set<Listener>();
 
 /**
  *****************************************
- * 创建订阅
- *****************************************
- */
-export function observer(shouldUpdate: () => boolean): Observer {
-    let listeners = new Set<Listener>(),
-        cancel: undefined | (() => void);
-
-    // 执行回调
-    function refresh(): void {
-
-        // 清空取消回调
-        cancel = undefined;
-
-        // 更新数据
-        if (shouldUpdate()) {
-            let list = listeners;
-
-            // 更新监听列表
-            listeners = new Set<Listener>();
-
-            // 执行事件
-            list.forEach(cb => {
-                if (cb.handler) {
-                    listeners.add(cb);
-                    cb.handler();
-                }
-            });
-        }
-    }
-
-    // 返回接口
-    return {
-        subscribe() {
-            stack.forEach(cb => listeners.add(cb));
-        },
-        notify() {
-            if (!cancel) {
-                cancel = rAF(refresh);
-            }
-        }
-    };
-}
-
-
-/**
- *****************************************
  * 添加监听
  *****************************************
  */
@@ -107,4 +61,104 @@ export function addListener(listener: Listener): void {
  */
 export function removeListener(listener: Listener): void {
     stack.delete(listener);
+}
+
+
+/**
+ *****************************************
+ * 任务队列
+ *****************************************
+ */
+interface Task {
+    list: Set<() => Listener[]>;
+    cancel?(): void;
+}
+
+
+/**
+ *****************************************
+ * 定义执行队列
+ *****************************************
+ */
+const task: Task = {
+    list: new Set(),
+    cancel: undefined
+};
+
+
+/**
+ *****************************************
+ * 执行任务队列
+ *****************************************
+ */
+function invoke(): void {
+    let list = task.list,
+        queue: Listener[] = [];
+
+    // 更新监听列表
+    task.list = new Set();
+
+    // 遍历任务
+    list.forEach(handler => {
+        let listeners = handler();
+
+        // 添加队列
+        if (listeners.length) {
+            queue = queue.concat(listeners);
+        }
+    });
+
+    // 执行队列
+    queue.forEach(cb => cb.handler && cb.handler());
+}
+
+
+/**
+ *****************************************
+ * 创建订阅
+ *****************************************
+ */
+export function observer(shouldUpdate: () => boolean): Observer {
+    let listeners = new Set<Listener>();
+
+    // 执行回调
+    function refresh(): Listener[] {
+        let queue: Listener[] = [];
+
+        // 更新数据
+        if (listeners.size && shouldUpdate()) {
+            let list = listeners;
+
+            // 更新监听列表
+            listeners = new Set<Listener>();
+
+            // 执行事件
+            list.forEach(cb => {
+                if (cb.handler) {
+                    listeners.add(cb);
+                    queue.push(cb);
+                }
+            });
+        }
+
+        // 返回结果
+        return queue;
+    }
+
+    // 返回接口
+    return {
+        subscribe() {
+            stack.forEach(cb => listeners.add(cb));
+        },
+        notify() {
+
+            // 启动任务
+            if (!task.cancel) {
+                task.cancel = rAF(invoke);
+            }
+
+            // 添加任务
+            task.list.add(refresh);
+        }
+    };
 }
